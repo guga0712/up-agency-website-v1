be43c65269adc0dd3b29920d173a37c0e04d0694
guga0712 <martins0712@hotmail.com>
2021-10-19 13:12:00 -0300

    added typewriter

---
+++ typing.js
@@ -0,0 +1,12 @@
+function typeWriteR(element){
+    const textArray = element.innerHTML.split('');
+    element.innerHTML = ''
+    textArray.forEach((letra, i) => {
+        setTimeout(function(){
+            element.innerHTML += letra
+        }, 75 * i)
+    })
+}
+
+const title = document.querySelector('.mainTitle');
+typeWriteR(title)
